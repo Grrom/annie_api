d37faa86a5c3b1e12e356cd682d8ffe8b0d8cb51
Jerome <josejeromelalunio@gmail.com>
2022-03-22 14:21:56 +0800

    send only one result for sauce

--- main.go
+++ main.go
@@ -7,22 +7,14 @@ import (
 )
 
 type sauce struct {
-	Database   string  `json:"database"`
-	Similarity float32 `json:"similarity"`
-	Author     string  `json:"author"`
-	Title      string  `json:"title"`
-}
-
-var books = []sauce{
-	{Database: "Pixiv", Similarity: 94.5, Author: "HEHE", Title: "sample sauce"},
-	{Database: "Pixiv", Similarity: 94.5, Author: "HEHE", Title: "sample sauce"},
-	{Database: "Pixiv", Similarity: 94.5, Author: "HEHE", Title: "sample sauce"},
-	{Database: "Pixiv", Similarity: 94.5, Author: "HEHE", Title: "sample sauce"},
-	{Database: "Pixiv", Similarity: 94.5, Author: "HEHE", Title: "sample sauce"},
+	Database string  `json:"database"`
+	Accuracy float64 `json:"accuracy"`
+	Author   string  `json:"author"`
+	Title    string  `json:"title"`
 }
 
 func getSauce(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, books)
+	c.IndentedJSON(http.StatusOK, sauce{Database: "Pixiv", Accuracy: 94.5, Author: "HEHE", Title: "sample sauce"})
 }
 
 func main() {
